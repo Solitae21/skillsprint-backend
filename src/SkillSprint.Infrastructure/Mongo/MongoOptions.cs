fbe750a87d76e1300ccb7f8a4fed896c492bc8c9
HansAmponin <amponinhans@gmail.com>
2026-08-03 08:10:58 +0800

    Add MongoDB configuration options and update Program.cs for dependency injection

---
+++ src/SkillSprint.Infrastructure/Mongo/MongoOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillSprint.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+public class MongoOptions
+{
+    [Required]
+    public string ConnectionString { get; set; } = default!;
+
+    [Required]
+    public string Database { get; set; } = default!;
+}
